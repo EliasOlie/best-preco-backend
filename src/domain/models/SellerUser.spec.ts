4786b574338ae4fa6d9b0442f6cbd97e06066325
EliasOlie <eliasvieira31@gmail.com>
2022-06-19 16:31:43 -0300

    Transport user model

--- src/domain/models/SellerUser.spec.ts
+++ src/domain/models/SellerUser.spec.ts
@@ -1,64 +1,70 @@
-import { SellerUser } from "./SellerUser"
+import { SellerUser } from "./SellerUser";
 
-describe('SellerUser Tests', () => {
+describe("SellerUser Tests", () => {
   let validPayload = {
-    "id": "123",
-    "userName": "Elias",
-    "userEmail": "a@b.com",
-    "userPassword": "1234",
-    "createdAt": "10/11/2011",
-    "modifiedAt": "10/11/2011",
-    "cnpj": "62.311.654/0001-93"
-  }
+    id: "123",
+    userName: "Elias",
+    userEmail: "a@b.com",
+    userPassword: "1234",
+    createdAt: "10/11/2011",
+    modifiedAt: "10/11/2011",
+    cnpj: "62.311.654/0001-93",
+  };
   test("Constructor valid CNPJ", () => {
-    new SellerUser(validPayload)
-  })
-  test("Constructor invalid CNPJ", () =>{
+    new SellerUser(validPayload);
+  });
+  test("Constructor invalid CNPJ", () => {
     let payload = {
-      "id": "123",
-      "userName": "Elias",
-      "userEmail": "a@b.com",
-      "userPassword": "1234",
-      "createdAt": "10/11/2011",
-      "modifiedAt": "10/11/2011",
-      "cnpj": "11.222.333/0001-01"
-    }
+      id: "123",
+      userName: "Elias",
+      userEmail: "a@b.com",
+      userPassword: "1234",
+      createdAt: "10/11/2011",
+      modifiedAt: "10/11/2011",
+      cnpj: "11.222.333/0001-01",
+    };
     const t = () => {
-      new SellerUser(payload)
-    }
-    expect(t).toThrow("Invalid CNPJ")
-  })
+      new SellerUser(payload);
+    };
+    expect(t).toThrow("Invalid CNPJ");
+  });
 
   describe("Update methods", () => {
-    let newUser = new SellerUser(validPayload)
+    let newUser = new SellerUser(validPayload);
     test("Update user name", () => {
-      newUser.updateUserName("Eliax")
-      expect(newUser.getUserName === "Eliax")
-    })
+      newUser.updateUserName("Eliax");
+      expect(newUser.getUserName === "Eliax");
+    });
 
     test("Update user email", () => {
-      newUser.updateUserEmail("a@c.com")
-      expect(newUser.getUserEmail === "a@c.co")
-    })
+      newUser.updateUserEmail("a@c.com");
+      expect(newUser.getUserEmail === "a@c.co");
+    });
 
     test("Update user password", () => {
-      newUser.updateUserPassword("123")
-      expect(newUser.getUserPassword === "123")
-    })
+      newUser.updateUserPassword("123");
+      expect(newUser.getUserPassword === "123");
+    });
 
     test("Update user modified at", () => {
-      newUser.updateModifiedAt("10/01/2022")
-      expect(newUser.getUserModifiedAt === "10/01/2022")
-    })
+      newUser.updateModifiedAt("10/01/2022");
+      expect(newUser.getUserModifiedAt === "10/01/2022");
+    });
 
     test("Update user name fail", () => {
-      expect(() => {newUser.updateUserName("Eliax")}).toThrow("You need to supply a new name, not the same")
-    })
+      expect(() => {
+        newUser.updateUserName("Eliax");
+      }).toThrow("You need to supply a new name, not the same");
+    });
     test("Update user email fail", () => {
-      expect(() => {newUser.updateUserEmail("a@c.com")}).toThrow("You need to supply a new email, not the same")
-    })
+      expect(() => {
+        newUser.updateUserEmail("a@c.com");
+      }).toThrow("You need to supply a new email, not the same");
+    });
     test("Update user password fail", () => {
-      expect(() => {newUser.updateUserPassword("123")}).toThrow("You need to supply a new password, not the same")
-    })
-  })
-})
+      expect(() => {
+        newUser.updateUserPassword("123");
+      }).toThrow("You need to supply a new password, not the same");
+    });
+  });
+});
--- src/domain/models/TransportUser.ts
+++ src/domain/models/TransportUser.ts
@@ -1,14 +1,67 @@
-import { BaseUser } from "./BaseUser";
+import { BuyerUser, BuyerUserDTO } from "./BuyerUser"
 
-class TransportUser extends BaseUser {
+class TransportUser extends BuyerUser {
     private cnh: string
     private placa: string
     private modelo: string
+    private isValidPlaca: boolean = false
 
-    public constructor(props: TransportUser){
+    public constructor(props: TransportUserDTO){
         super(props)
         Object.assign(this, props)
     }
+
+    public get getCnh() {
+        return this.cnh
+    }
+
+    public updateCnh(newCnh: string) {
+        if(newCnh.toLocaleLowerCase() === this.cnh.toLocaleLowerCase()){
+            throw new Error("You need to supply a new cnh, not the same")
+        }else{
+            this.cnh = newCnh
+        }
+    }
+
+    public get getPlaca() {
+        return this.placa
+    }
+
+    public updatePlaca(newPlaca: string) {
+        if(newPlaca.toLocaleLowerCase() === this.placa.toLocaleLowerCase()){
+            throw new Error("You need to supply a new placa, not the same")
+        }else{
+            this.placa = newPlaca
+        }
+    }
+
+    public get getModelo() {
+        return this.modelo
+    }
+
+    public updateModelo(newModelo: string) {
+        if(newModelo.toLocaleLowerCase() === this.modelo.toLocaleLowerCase()){
+            throw new Error("You need to supply a new modelo, not the same")
+        }else{
+            this.modelo = newModelo
+        }
+
+    }
+
+    public get getIsValidPlaca() {
+        return this.isValidPlaca
+    }
+
+    public updateIsValidPlaca(){
+        this.isValidPlaca = !this.isValidPlaca
+    }
+}
+
+interface TransportUserDTO extends BuyerUserDTO{
+    cnh: string
+    placa: string
+    modelo: string
+    isValidPlaca?: boolean
 }
 
 export { TransportUser }
