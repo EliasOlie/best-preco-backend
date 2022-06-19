d65260eeb91c968011a8098f923677739f54a12c
EliasOlie <eliasvieira31@gmail.com>
2022-06-19 16:29:57 -0300

    Buyer user tests

---
+++ src/domain/models/BuyerUser.spec.ts
@@ -0,0 +1,31 @@
+import { BuyerUser } from "./BuyerUser";
+
+describe("Buyer user tests", () => {
+    let validPayload =  {
+    id: "123",
+    userName: "Elias",
+    userEmail: "a@b.com",
+    userPassword: "1234",
+    createdAt: "10/11/2011",
+    modifiedAt: "10/11/2011",
+    cpf: "391.360.920-29",
+  };
+  test("Constructor", () => {
+    new BuyerUser(validPayload)
+  })
+  test("Constructor fail", () => {
+    let invalidPayload =  {
+        id: "123",
+        userName: "Elias",
+        userEmail: "a@b.com",
+        userPassword: "1234",
+        createdAt: "10/11/2011",
+        modifiedAt: "10/11/2011",
+        cpf: "123.456.789-01",
+      };
+    expect(() => {
+        new BuyerUser(invalidPayload)
+    }).toThrow("Invalid CPF")
+  })  
+
+})
