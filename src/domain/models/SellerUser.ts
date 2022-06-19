6a7c8990248266ad8d630907a42710a8a8c914c4
EliasOlie <eliasvieira31@gmail.com>
2022-06-19 15:29:20 -0300

    Seller user model enriched and tested

--- src/domain/models/SellerUser.ts
+++ src/domain/models/SellerUser.ts
@@ -1,12 +1,93 @@
-import { BaseUser } from "./BaseUser";
+import { BaseUser, BaseUserDTO } from "./BaseUser";
 
 class SellerUser extends BaseUser {
-    private cnpj: string
+  private cnpj: string;
 
-    public constructor(props: SellerUser){
-        super(props)
-        Object.assign(this, props)
+  public constructor(props: SellerUserDTO) {
+    super(props);
+    if (this.cnpjValidation(props.cnpj)) {
+      Object.assign(this, props);
+    } else {
+      throw new Error("Invalid CNPJ");
     }
+  }
+
+  private cnpjValidation(value: string) {
+    //gist https://gist.github.com/alexbruno/6623b5afa847f891de9cb6f704d86d02
+    if (!value) return false;
+
+    // Aceita receber o valor como string, número ou array com todos os dígitos
+    const isString = typeof value === "string";
+    const validTypes =
+      isString || Number.isInteger(value) || Array.isArray(value);
+
+    // Elimina valor em formato inválido
+    if (!validTypes) return false;
+
+    // Filtro inicial para entradas do tipo string
+    if (isString) {
+      // Limita ao máximo de 18 caracteres, para CNPJ formatado
+      if (value.length > 18) return false;
+
+      // Teste Regex para veificar se é uma string apenas dígitos válida
+      const digitsOnly = /^\d{14}$/.test(value);
+      // Teste Regex para verificar se é uma string formatada válida
+      const validFormat = /^\d{2}.\d{3}.\d{3}\/\d{4}-\d{2}$/.test(value);
+
+      // Se o formato é válido, usa um truque para seguir o fluxo da validação
+      if (digitsOnly || validFormat) true;
+      // Se não, retorna inválido
+      else return false;
+    }
+
+    // Guarda um array com todos os dígitos do valor
+    const match = value.toString().match(/\d/g);
+    const numbers = Array.isArray(match) ? match.map(Number) : [];
+
+    // Valida a quantidade de dígitos
+    if (numbers.length !== 14) return false;
+
+    // Elimina inválidos com todos os dígitos iguais
+    const items = [...new Set(numbers)];
+    if (items.length === 1) return false;
+
+    // Cálculo validador
+    const calc = (x: number) => {
+      const slice = numbers.slice(0, x);
+      let factor = x - 7;
+      let sum = 0;
+
+      for (let i = x; i >= 1; i--) {
+        const n = slice[x - i];
+        sum += n * factor--;
+        if (factor < 2) factor = 9;
+      }
+
+      const result = 11 - (sum % 11);
+
+      return result > 9 ? 0 : result;
+    };
+
+    // Separa os 2 últimos dígitos de verificadores
+    const digits = numbers.slice(12);
+
+    // Valida 1o. dígito verificador
+    const digit0 = calc(12);
+    if (digit0 !== digits[0]) return false;
+
+    // Valida 2o. dígito verificador
+    const digit1 = calc(13);
+    return digit1 === digits[1];
+  }
+
+  public get getCNPJ() {
+    return this.cnpj
+  }
+
 }
 
-export { SellerUser }
+interface SellerUserDTO extends BaseUserDTO {
+  cnpj: string;
+}
+
+export { SellerUser };
